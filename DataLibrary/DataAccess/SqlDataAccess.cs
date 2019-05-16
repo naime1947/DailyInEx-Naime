b4ec82ddf8d845e5b6720d7d0d7944a1da57921e
Naime Ahmed <naime1947@gmail.com>
2019-05-16 23:23:31 +0600

    SQLDataAccess Class Created. There are three method. 1. is for get connectionstring 1. Load Data and 3 is Save Data

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using Dapper;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        public static string GetConnectionString(string connectionName = "DailyInExDataBase")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using(IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+    }
+}
